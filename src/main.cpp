18a5fc69383bd16bd395964f976276cc1a92870e
Grzetan <grzetan@gmail.com>
2021-11-01 11:45:40 +0100

    Implemented user's function preprocessing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,59 @@
 #include <iostream>
+#include <algorithm>
 #include "Complex.h"
 
+std::string fx = "0", fprime = "0", multi, pow, sign;
+
+void preprocessChunk(std::string chunk){
+    if(chunk.find('z') != std::string::npos){
+        multi = chunk.substr(1, chunk.find('z')-1);
+        if(multi == ""){
+            multi = "1";
+        }
+        sign = chunk[0];
+        if(chunk.find('^') != std::string::npos){
+            pow = chunk.substr(chunk.find('^')+1, chunk.length() - 1);
+            fx = fx + sign + "(z^" + pow + ")*" + multi;
+            fprime = fprime + sign + "(z^" + std::to_string(stoi(pow) - 1) + ")*" + std::to_string(stoi(multi) * stoi(pow));
+        }else{
+            fx = fx + sign + "z*" + multi;
+            fprime = fprime + sign + multi;
+        }
+    }else{
+        fx = fx + chunk;
+    }
+}
+
+void processEquation(std::string equation){
+    int lastChunk = 0;
+    if(equation[0] != '-'){
+        equation = "+" + equation;
+    }
+    for(int i=0; i<equation.length(); i++){
+        if(equation[i] == '+' || equation[i] == '-'){
+            preprocessChunk(equation.substr(lastChunk, i - lastChunk));
+            lastChunk = i;
+        }
+    }
+    preprocessChunk(equation.substr(lastChunk, equation.length()));
+}
+
+void compile(std::string equation){
+    
+}
+
 int main(int argc, char *argv[]){
-    Complex x(-0.9,3.6);
-    Complex z = (x^3) * 16 + 2000;
-    z.print();
+    std::string equation = argv[1];
+
+    //Remove spaces
+    equation.erase(std::remove(equation.begin(), equation.end(), ' '), equation.end());
+
+    //Preprocess equation
+    processEquation(equation);
+    std::cout << fx << std::endl;
+    std::cout << fprime << std::endl;
+    //Compile
+    compile(equation);
+
     return 0;
 }
